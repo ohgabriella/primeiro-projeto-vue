6421b4653054a01ca178f200a47a8915f960a0fe
Gabriella Barreto <gabriellabarretogomes@gmail.com>
2019-05-28 10:28:19 -0300

    Integração com a API

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img v-bind:src="foto[0].url" v-bind:alt="foto[0].titulo">
-    <img src="https://levesemdestino.com/wp-content/uploads/2018/09/bolivia-o-que-saber-antes-de-ir-mochilao-chile-bolivia-peru-america-do-sul-uyuni-atacama.jpg" alt="Bolívia">
+    <ul>
+      <li v-for="foto of fotos">
+       <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+      </li>
 
+    </ul>
   </div>
 </template>
 
@@ -12,17 +15,15 @@ export default {
   data(){
     return{
       titulo: 'Alurapic',
-      foto: [
-        {
-        url: 'https://media.istockphoto.com/photos/presidential-palace-in-asuncion-paraguay-picture-id525336822?k=6&m=525336822&s=612x612&w=0&h=M_EjzCwczCG5DbJHT83ZNStFa0sfLZmPv8uELCUU7wc=',
-        titulo: 'Paraguay'
-        }
-                
-        ]
+      fotos: []
     }
+  },
 
-  }
-  
+  created(){
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then( res =>  res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+    }
 }
 </script>
 
